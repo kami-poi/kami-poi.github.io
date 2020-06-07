ad294a89e50e0b803e310009d4bcb3481eb42870
kami-poi <2214966912@qq.com>
2020-06-07 15:49:37 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git clone --depth 1 --branch=master https://github.com/kami-poi/kami-poi.github.
 
 cd .deploy_git
 
-＃从 ../public/ 复制之前删除所有文件
+#从 ../public/ 复制之前删除所有文件
 # 这样 git 可以跟踪上次提交中删除的文件
 find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
 
